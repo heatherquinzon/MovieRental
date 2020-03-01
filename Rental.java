ab3fd24d154898c2b60592d4ce212c3217d4e7d9
Aaron Cherian <47262140+cherianaaron@users.noreply.github.com>
2020-03-01 15:57:58 -0600

    Test case for daysRented >=1

--- Rental.java
+++ Rental.java
@@ -2,16 +2,22 @@ package org.acm.fowler;
 
 class Rental {
     private Movie _movie;
-    private int _daysRented = 0;
+    private int _daysRented;
 
     public Rental(Movie movie, int daysRented) {
         _movie = movie;
+        if (daysRented >=1){
         _daysRented = daysRented;
+        }
+        else{
+            System.out.println("Error, Please enter a value greater or equal to 1");
+        }
     }
 
     public int getDaysRented() {
         return _daysRented;
     }
+    
 
     public Movie getMovie() {
         return _movie;  
