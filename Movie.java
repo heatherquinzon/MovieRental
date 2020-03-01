81b7be9ef71f72a6b9ac04691cb31f6175a10ccf
Aaron Cherian <47262140+cherianaaron@users.noreply.github.com>
2020-03-01 15:57:18 -0600

    Added test cases for setPrice and default const

--- Movie.java
+++ Movie.java
@@ -1,33 +1,38 @@
 package org.acm.fowler;
 
 public class Movie {
-
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
-    private String _title = "Not_Available";
-    private int _priceCode = 0;
+    private String _title;
+    private int _priceCode;
 
 
     public Movie(String title, int priceCode) {
         _title = title;
+        if(priceCode >=0 && priceCode <=2){
         _priceCode = priceCode;
+        }
+        else{
+            System.out.println("Error, Not valid pricecode. Please enter 0, 1, or 2");
+        }
     }
 
     public int getPriceCode() {
         return _priceCode;
     }
 
-    public void setPriceCode(int code) {
-        if(code >=0 && code <=2){
-        _priceCode = code;
+    public void setPriceCode(int priceCode) {
+        if(priceCode >=0 && priceCode <=2){ //testing problem
+        _priceCode = priceCode;
         }
         else{
-        System.out.println(“Error, Please enter either 0, 1 or 2”);
+        System.out.println("Error, Not valid pricecode. Please enter 0, 1, or 2");
         }
     }
 
     public String getTitle() {
         return _title;
     }
+   
 };
